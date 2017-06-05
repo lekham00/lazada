1fffa7681a6a81804d3d3d8f3ae9547ee78ad0eb
DESKTOP-SJ4E3E3\Le Kham <lekhamcntt09@gmail.com>
2017-06-06 00:39:30 +0700

    up code

---
+++ app/src/main/java/com/example/lekham/lazada/CustomView/CustomEditText.java
@@ -0,0 +1,147 @@
+package com.example.lekham.lazada.CustomView;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.icu.util.MeasureUnit;
+import android.os.Build;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.content.ContextCompat;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.lekham.lazada.R;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by Le Kham on 6/3/2017.
+ */
+
+@SuppressLint("AppCompatCustomView")
+public class CustomEditText extends EditText implements View.OnFocusChangeListener {
+    Drawable mClearImages, mNoneClearImages;
+    Drawable drawableCLear;
+    Boolean mVisible = false;
+    boolean mUseEmail = false;
+    boolean mUseError = false;
+    String mTextErrorEmpty;
+    String mTextErrorEmailInvalid;
+    int ALPHA = (int) (255 * .20f);
+    TextInputLayout mTextInputLayout;
+
+    public CustomEditText(Context context) {
+        super(context);
+        init(null);
+    }
+
+    public CustomEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public CustomEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public CustomEditText(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(attrs);
+    }
+
+    private void init(AttributeSet attributeSet) {
+        if (attributeSet != null) {
+            TypedArray typedArray = getContext().getTheme().obtainStyledAttributes(attributeSet, R.styleable.CustomEditText, 0, 0);
+            mUseEmail = typedArray.getBoolean(R.styleable.CustomEditText_useEmail, false);
+            mUseError = typedArray.getBoolean(R.styleable.CustomEditText_useError, false);
+            mTextErrorEmpty = typedArray.getString(R.styleable.CustomEditText_textErrorEmpty);
+            mTextErrorEmailInvalid = typedArray.getString(R.styleable.CustomEditText_textErrorEmailInvalid);
+        }
+        mClearImages = ContextCompat.getDrawable(getContext(), R.drawable.ic_clear_black_24dp).mutate();
+        mNoneClearImages = ContextCompat.getDrawable(getContext(), android.R.drawable.screen_background_light_transparent).mutate();
+        setUp();
+        this.setOnFocusChangeListener(this);
+    }
+
+    private void setUp() {
+        setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_WEB_EMAIL_ADDRESS);
+        Drawable[] drawable = getCompoundDrawables();
+        drawableCLear = mVisible ? mClearImages : mNoneClearImages;
+        if (mVisible)
+            drawableCLear.setAlpha(ALPHA);
+        setCompoundDrawablesWithIntrinsicBounds(drawable[0], drawable[1], drawableCLear, drawable[3]);
+    }
+
+    @Override
+    protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
+        super.onTextChanged(text, start, lengthBefore, lengthAfter);
+        Log.d("AAA", "start :" + start + "- lengthBefore:" + lengthBefore + "-lengthAfter:" + lengthAfter);
+        handelCloseButton(start, lengthAfter);
+        handelError(start, lengthBefore, text);
+
+    }
+
+    private void handelCloseButton(int start, int lengthAfter) {
+        if (lengthAfter == 0 && start == 0) {
+            mVisible = false;
+            setUp();
+        } else {
+            mVisible = true;
+            setUp();
+        }
+    }
+
+    private void handelError(int start, int lengthBefore, CharSequence text) {
+        if (mUseError && !TextUtils.isEmpty(mTextErrorEmpty)) {
+            if (start <= 0 && lengthBefore != 0) {
+                mTextInputLayout.setErrorEnabled(true);
+                mTextInputLayout.setError(mTextErrorEmpty);
+            } else if (mUseEmail && !TextUtils.isEmpty(mTextErrorEmailInvalid)) {
+                boolean isCheckEmail = Patterns.EMAIL_ADDRESS.matcher(text).matches();
+                if (!isCheckEmail) {
+                    mTextInputLayout.setErrorEnabled(true);
+                    mTextInputLayout.setError(mTextErrorEmailInvalid);
+                } else {
+                    mTextInputLayout.setErrorEnabled(false);
+                    mTextInputLayout.setError(null);
+                }
+            } else {
+                mTextInputLayout.setErrorEnabled(false);
+                mTextInputLayout.setError(null);
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN && event.getX() >= (getRight() - drawableCLear.getBounds().width())) {
+            setText("");
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if (mUseError && !TextUtils.isEmpty(mTextErrorEmpty)) {
+            mTextInputLayout = (TextInputLayout) v.getParent().getParent();
+            if (!hasFocus) {
+                if (TextUtils.isEmpty(getText())) {
+                    mTextInputLayout.setErrorEnabled(true);
+                    mTextInputLayout.setError(mTextErrorEmpty);
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/lekham/lazada/CustomView/EditTextPassword.java
+++ app/src/main/java/com/example/lekham/lazada/CustomView/EditTextPassword.java
@@ -6,14 +6,20 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.content.ContextCompat;
 import android.text.InputType;
+import android.text.TextUtils;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 
 import com.example.lekham.lazada.R;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Le Kham on 6/2/2017.
@@ -21,12 +27,23 @@ import com.example.lekham.lazada.R;
 
 
 @SuppressLint("AppCompatCustomView")
-public class EditTextPassword extends EditText {
+public class EditTextPassword extends EditText implements View.OnFocusChangeListener {
     Drawable drawable;
     Drawable mEye, mEyeStrike;
-    Boolean mVisible = false;
-    Boolean mUseStrike = false;
+    boolean mVisible = false;
+    boolean mUseStrike = false;
+    boolean mUserError = false;
+    int mTextSizeMin = 0;
+    int mTextSizeMax = 0;
+    String mTextErrorEmpty;
+    String mTextErrorSizeMinInvalid;
+    String mTextErrorSizeMaxInvalid;
+    String mTextErrorTextInvalid;
     int ALPHA = (int) (255 * .20f);
+    String MATCHER_PATTERN = "((?=.*\\d)(?=.*[A-Z])(?=.*[a-z]).{6,20})";
+    Pattern mPattern;
+    Matcher mMatcher;
+    TextInputLayout mTextInputLayout;
 
     public EditTextPassword(Context context) {
         super(context);
@@ -53,14 +70,23 @@ public class EditTextPassword extends EditText {
         if (attrs != null) {
             TypedArray typedArray = getContext().getTheme().obtainStyledAttributes(attrs, R.styleable.EditTextPassword, 0, 0);
             mUseStrike = typedArray.getBoolean(R.styleable.EditTextPassword_useStrike, false);
+            mUserError = typedArray.getBoolean(R.styleable.EditTextPassword_useErrorPass, false);
+            mTextErrorEmpty = typedArray.getString(R.styleable.EditTextPassword_textErrorPassEmpty);
+            mTextErrorSizeMinInvalid = typedArray.getString(R.styleable.EditTextPassword_textErrorSizeMinInvalid);
+            mTextErrorSizeMaxInvalid = typedArray.getString(R.styleable.EditTextPassword_textErrorSizeMaxInvalid);
+            mTextErrorTextInvalid = typedArray.getString(R.styleable.EditTextPassword_textErrorTextInvalid);
+            mTextSizeMin = typedArray.getInt(R.styleable.EditTextPassword_textSizeMin, 0);
+            mTextSizeMax = typedArray.getInt(R.styleable.EditTextPassword_textSizeMax, 0);
         }
         mEye = ContextCompat.getDrawable(getContext(), R.drawable.ic_visibility_black_24dp).mutate();
         mEyeStrike = ContextCompat.getDrawable(getContext(), R.drawable.ic_visibility_off_black_24dp).mutate();
         setUp();
+        this.setOnFocusChangeListener(this);
+
     }
 
     private void setUp() {
-
+        mPattern = Pattern.compile(MATCHER_PATTERN);
         setInputType(InputType.TYPE_CLASS_TEXT | (mVisible ? InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD : InputType.TYPE_TEXT_VARIATION_PASSWORD));
         Drawable[] drawables = getCompoundDrawables();
         drawable = mUseStrike && !mVisible ? mEyeStrike : mEye;
@@ -77,5 +103,52 @@ public class EditTextPassword extends EditText {
         }
 
         return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if (mUserError && !TextUtils.isEmpty(mTextErrorEmpty)) {
+            mTextInputLayout = (TextInputLayout) v.getParent().getParent();
+            if (!hasFocus) {
+                String text = getText().toString();
+                if (TextUtils.isEmpty(text)) {
+                    mTextInputLayout.setErrorEnabled(true);
+                    mTextInputLayout.setError(mTextErrorEmpty);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
+        super.onTextChanged(text, start, lengthBefore, lengthAfter);
+        if (mUserError && mTextInputLayout != null && mTextSizeMax > 0
+                && mTextSizeMin > 0 && !TextUtils.isEmpty(mTextErrorSizeMaxInvalid)
+                && !TextUtils.isEmpty(mTextErrorSizeMinInvalid) && !TextUtils.isEmpty(mTextErrorTextInvalid)
+                && !TextUtils.isEmpty(mTextErrorEmpty)) {
+            if (lengthAfter != lengthBefore) {
+                if (start <= 0 && lengthBefore != 0) {
+                    mTextInputLayout.setErrorEnabled(true);
+                    mTextInputLayout.setError(mTextErrorEmpty);
+                } else if (start < mTextSizeMin) {
+                    mTextInputLayout.setErrorEnabled(true);
+                    mTextErrorSizeMinInvalid = mTextErrorSizeMinInvalid.replace("%", String.valueOf(mTextSizeMin));
+                    mTextInputLayout.setError(mTextErrorSizeMinInvalid);
+                } else if (start > mTextSizeMax) {
+                    mTextInputLayout.setErrorEnabled(true);
+                    mTextErrorSizeMaxInvalid = mTextErrorSizeMaxInvalid.replace("%", String.valueOf(mTextSizeMax));
+                    mTextInputLayout.setError(mTextErrorSizeMaxInvalid);
+                } else if (start >= mTextSizeMin && start <= mTextSizeMax) {
+                    mMatcher = mPattern.matcher(text);
+                    if (!mMatcher.matches()) {
+                        mTextInputLayout.setErrorEnabled(true);
+                        mTextInputLayout.setError(mTextErrorTextInvalid);
+                    } else {
+                        mTextInputLayout.setErrorEnabled(false);
+                        mTextInputLayout.setError(null);
+                    }
+                }
+            }
+        }
     }
 }
